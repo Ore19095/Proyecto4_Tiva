b730d36d0a31648a3ee6befb7bac353d3334baa2
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-05-15 02:56:10 -0600

    7 SEGMENTOS LISTO

---
+++ Debug/main.d
@@ -0,0 +1,54 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+main.obj: D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+main.obj: D:/TivaWare/inc/hw_memmap.h
+main.obj: D:/TivaWare/inc/hw_types.h
+main.obj: D:/TivaWare/inc/hw_gpio.h
+main.obj: D:/TivaWare/driverlib/sysctl.h
+main.obj: D:/TivaWare/driverlib/gpio.h
+
+../main.c:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+D:/programas/codeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+D:/TivaWare/inc/hw_memmap.h:
+
+D:/TivaWare/inc/hw_types.h:
+
+D:/TivaWare/inc/hw_gpio.h:
+
+D:/TivaWare/driverlib/sysctl.h:
+
+D:/TivaWare/driverlib/gpio.h:
+
--- main.c
+++ main.c
@@ -1,9 +1,58 @@
-
-
 /**
- * main.c
+ * ANGEL ORELLANA
+ * PROYECTO 4
+ * PARQUEO MATIC: SISTEMA DE ESTACIONAMIENTOS
  */
-int main(void)
-{
+#include <stdint.h>
+#include <stdbool.h>
+#include <inc/hw_memmap.h>
+#include <inc/hw_types.h>
+#include <inc/hw_gpio.h>
+#include <driverlib/sysctl.h>
+#include <driverlib/gpio.h>
+
+// variable que se usara para el 7 segmentos
+static uint8_t tabla[] = {
+                        0b11100111, // 0
+                        0b10000010, // 1
+                        0b01101011, // 2
+                        0b11001011, //3
+                        0b10001110 //4
+};
+uint8_t parqueosDisponibles;
+
+int main(void){
+    // configuracion: reloj con entrada principal de 16MHZ usando pll con una frecuencia de 40MHZ
+    SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ| SYSCTL_SYSDIV_5);
+
+    // conectando los puertos al reloj del sistema :D
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // ENTRADAS DE LOS SENSORES(DIP SWITCH)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); // SALIDA PARA EL 7 SEGMENTOS
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); // SALIDA PARA LOS LEDS
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // SALIDA PARA EL 7 SEGMENTOS
+    //configurar los pines
+    HWREG(GPIO_PORTD_BASE+GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTD_BASE+GPIO_O_CR) |= GPIO_PIN_7;
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_5); // para el 7 segmentos
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
+                                           GPIO_PIN_3 |GPIO_PIN_4 | GPIO_PIN_5 |
+                                           GPIO_PIN_6 | GPIO_PIN_7); // para las luces de encendido
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
+                                           GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7); // para llos 7 segmentos
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2 |
+                                         GPIO_PIN_3 |GPIO_PIN_4 );
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2 |
+                                         GPIO_PIN_3 |GPIO_PIN_4, ui32Strength, ui32PadType)
+
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
+                                               GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7, tabla[4]);
+
+    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, tabla[4]);
+    while(1){
+
+
+
+    }
+
 	return 0;
 }
