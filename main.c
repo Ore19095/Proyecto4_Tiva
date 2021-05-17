e09f0a1cefd119b10af32719ce32c4a58e6319ec
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-05-17 15:38:47 -0600

    falta comunicacion serial entre tiva y esp32

--- main.c
+++ main.c
@@ -20,7 +20,9 @@ static uint8_t tabla[] = {
                         0b10001110 //4
 };
 uint8_t parqueosDisponibles;
-
+uint8_t complementoParqueos;
+uint8_t noParqueos;
+uint8_t lucesParqueos;
 int main(void){
     // configuracion: reloj con entrada principal de 16MHZ usando pll con una frecuencia de 40MHZ
     SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ| SYSCTL_SYSDIV_5);
@@ -40,17 +42,43 @@ int main(void){
     GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
                                            GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7); // para llos 7 segmentos
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2 |
-                                         GPIO_PIN_3 |GPIO_PIN_4 );
+                                         GPIO_PIN_3 |GPIO_PIN_4 );// las entradas de los sensores
     GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2 |
-                                         GPIO_PIN_3 |GPIO_PIN_4, ui32Strength, ui32PadType)
+                                         GPIO_PIN_3 |GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);// weak pull up
 
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
-                                               GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7, tabla[4]);
+    /*GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
+                                               GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7, tabla[3]);
 
-    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, tabla[4]);
+
+    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, tabla[4]);*/
+    int i = 0;
+
     while(1){
+        lucesParqueos = 0;
+        parqueosDisponibles = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2 |
+                                         GPIO_PIN_3 |GPIO_PIN_4);
+        // se obtienen la cantidad de parqueos disponibles
+        noParqueos = ((parqueosDisponibles>>1) & 1) + ((parqueosDisponibles>>2)&1) +((parqueosDisponibles>>3)&1) + ((parqueosDisponibles>>4)&1);
+        // indicadores para parqueos;
+        if(parqueosDisponibles&2) lucesParqueos |= 1;
+        else lucesParqueos|= 2;
 
+        if(parqueosDisponibles&4) lucesParqueos |= 4;
+        else lucesParqueos|= 8;
 
+        if(parqueosDisponibles&8) lucesParqueos |= 16;
+        else lucesParqueos|= 32;
+
+        if(parqueosDisponibles&16) lucesParqueos |= 64;
+        else lucesParqueos|= 128;
+
+        //noParqueos = 4>>1;
+        GPIOPinWrite(GPIO_PORTB_BASE,255 ,lucesParqueos);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1| GPIO_PIN_2 |
+                                                       GPIO_PIN_3 |GPIO_PIN_6 | GPIO_PIN_7, tabla[noParqueos]);
+
+        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, tabla[noParqueos]);
+        //SysCtlDelay(200000);
 
     }
 
